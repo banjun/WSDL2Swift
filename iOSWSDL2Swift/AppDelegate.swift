c4c086cd8260c1b32675cc738904053e7b9cf6a4
BAN Jun <banjun@gmail.com>
2016-11-22 17:39:53 +0900

    add empty single view ios app for example base

---
+++ iOSWSDL2Swift/AppDelegate.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UINavigationController(rootViewController: ViewController(nibName: nil, bundle: nil))
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+    
+}
---
+++ iOSWSDL2Swift/ViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "WSDL2Swift Example"
+        view.backgroundColor = .white
+    }
+}
