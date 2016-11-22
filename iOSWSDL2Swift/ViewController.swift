73c122047f42380ef7e4d79ad460a62b82c7785a
BAN Jun <banjun@gmail.com>
2016-11-22 17:52:43 +0900

    add example usage in ViewController (may not build as it depends on exampleWSDLs)

--- iOSWSDL2Swift/ViewController.swift
+++ iOSWSDL2Swift/ViewController.swift
@@ -5,5 +5,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         title = "WSDL2Swift Example"
         view.backgroundColor = .white
+
+        // codes depend on exampleWSDLs you place
+        let auth = AuthenticationServiceService(endpoint: "https://examle.com")
+        let login = auth.request(AuthenticationServiceService_login(arg0: "alice@example.com", arg1: "password"))
+        login.onComplete { r in
+            NSLog("%@", "result = \(r)")
+        }
     }
 }
